20155e62a8b63e97ea19b631968ccc43c67583c4
dima-uryvskiyy <dima-uryvskiyy@rambler.ru>
2020-01-30 01:47:09 +0300

    Create repository

---
+++ Football/Example.py
@@ -0,0 +1,39 @@
+import requests
+import re
+from lxml import html
+import smtplib
+
+
+def print_match():
+    print(parsed_body.xpath('//title/text()')[0])  # title страницы
+    index = 0
+    index_t = 0
+    while index < len(clubs):
+        print(f"Дата: {date[index_t]}  Время: {time[index_t]}" if index_t < len(time) else f"Дата: {date[index_t]}", end='\n')
+        print(f"{clubs[index]}  - : -  {clubs[index + 1]}")
+        index += 2
+        index_t += 1
+
+
+response = requests.get('https://www.sports.ru/la-liga/calendar/')
+
+
+"""
+with open('test.html', 'w') as f:
+    f.write(response.text)
+"""
+
+# Преобразование тела документа в дерево элементов (DOM)
+parsed_body = html.fromstring(response.text)
+
+clubs = parsed_body.xpath('//tbody//tr//td//a//@title')
+date = re.findall("\d{2}\.\d{2}\.\d{4}", ' '.join(parsed_body.xpath('//tbody//tr//td//a//text()')))
+time = re.findall("\d{2}:\d{2}", ' '.join(parsed_body.xpath('//tbody//tr//td//a//text()')))
+
+print_match()
+
+smtpObj = smtplib.SMTP('smtp..ru', 587)
+smtpObj.starttls()
+smtpObj.login('dima_uryvskyi_footbol@rambler.ru', 'Vqj4P6cqLZgYw')
+smtpObj.sendmail("dima_uryvskyi_footbol@rambler.ru", "uryvskydmitry@yandex.ru", "go to bed!")
+smtpObj.quit()
