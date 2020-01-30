ed8a2930856d240c5b12235b7578632c89284523
dima-uryvskyi <dima-uryvskiyy@rambler.ru>
2020-01-31 02:26:16 +0300

    Updated solution test task

--- Tests/Task 3.py
+++ Tests/Task 3.py
@@ -6,12 +6,5 @@ driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://www.google.com/search?q=xpath')
 page = driver.page_source
 parsed_body = html.fromstring(page)
-"""
-print(*parsed_body.xpath('//div[@class="r"]//a[contains(@ping,"/url?") '
-                         'and not(contains(@href,"google"))]/@href'), sep='\n')
 
-"""
-
-
-print(*parsed_body.xpath('//a[contains(@ping,"/url?") '
-                         'and not(contains(@href,"google"))]/@href'), sep='\n')
+print(*parsed_body.xpath('//div[@class="rc"]/div[@class="r"]/a/@href'), sep='\n')
