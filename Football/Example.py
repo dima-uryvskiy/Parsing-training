bf2b8013414c4c4e86367bac18a806e909b34a25
dima-uryvskyi <dima-uryvskiyy@rambler.ru>
2020-02-15 23:12:04 +0300

    Add time, date and clubs

--- Football/Example.py
+++ Football/Example.py
@@ -6,16 +6,26 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
+
+
 def look_match():
+    response = requests.get('https://www.sports.ru/la-liga/calendar/')
+    parsed_body = html.fromstring(response.text)
+
+    time = parsed_body.xpath("//tbody/tr/td[@class=\"name-td alLeft\"]/a/text()")
+    owner_clubs = parsed_body.xpath("(//tbody/tr/td[@class=\"owner-td\"]//a[@class=\"player\"]/text())")
+    guests_clubs = parsed_body.xpath("(//tbody/tr/td[@class=\"guests-td\"]//a[@class=\"player\"]/text())")
+    print(len(time))
+    print(len(owner_clubs))
     index = 0
-    index_t = 0
     with open('timetable.txt', 'w') as fp:
         fp.write(parsed_body.xpath('//title/text()')[0] + '\n')  # title страницы
-        while index < len(clubs):
-            fp.write(f"Дата: {date[index_t]}  Время: {time[index_t]} \n" if index_t < len(time) else f"Дата: {date[index_t]}\n")
-            fp.write(f" {clubs[index]}  - : -  {clubs[index + 1]}\n")
-            index += 2
-            index_t += 1
+        while index < len(owner_clubs):
+            fp.write(parsed_body.xpath('(//h3)[1]/text()')[0] + '\n')
+            fp.write(f"Дата: {time[index]}  Время: {time[index  + 1]}" + " \n")
+            fp.write(f"{owner_clubs[index]} : {guests_clubs[index]}" + " \n")
+            index += 1
+
 
 
 def send_message(email_from, email_to):
@@ -30,19 +40,6 @@ def send_message(email_from, email_to):
     smtpObj.sendmail(email_from, email_to, msg.as_string())
     smtpObj.quit()
 
-
-response = requests.get('https://www.sports.ru/la-liga/calendar/')
-
-"""
-with open('test.html', 'w') as f:
-    f.write(response.text)
-"""
-
-# Преобразование тела документа в дерево элементов (DOM)
-parsed_body = html.fromstring(response.text)
-clubs = parsed_body.xpath('//tbody//tr//td//a//@title')
-date = re.findall("\d{2}\.\d{2}\.\d{4}", ' '.join(parsed_body.xpath('//tbody//tr//td//a//text()')))
-time = re.findall("\d{2}:\d{2}", ' '.join(parsed_body.xpath('//tbody//tr//td//a//text()')))
 
 look_match()
 #send_message("test.football.dima@gmail.com", "nagrdnk2017@mail.ru")
