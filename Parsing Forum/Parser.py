28f0442c634ea451891b7127f2da7a43678601d3
dima-uryvskyi <dima-uryvskiyy@rambler.ru>
2020-02-22 23:31:56 +0300

    Updated

--- Parsing Forum/Parser.py
+++ Parsing Forum/Parser.py
@@ -1,9 +1,10 @@
-from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
 
 url = "http://forum.eve-ru.com/index.php?showtopic=111891"
 response = requests.get(url)
 
-soap = BeautifulSoup(response.text)
-
+soap = BeautifulSoup(response.text, "html.parser")
+messages = soap.select("div.post.entry-content")
+parsed_messages = [msg.get_text() for msg in messages]
+print(*parsed_messages)
