75d73d02fd8548fc8457cb8bb8c6082eb18ebf65
dima-uryvskyi <dima-uryvskiyy@rambler.ru>
2020-02-24 03:48:39 +0300

    Create files witch products

--- Parsing Avito/Parser Avito.py
+++ Parsing Avito/Parser Avito.py
@@ -1,5 +1,4 @@
 import csv
-
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -22,24 +21,40 @@ def get_page_data(html):
     write_in_file(info_product)
 
 
-def write_in_file(info_phone):
-    with open("info_product.csv", "w") as file:
-        for data in info_phone:
-            name = data.find("a", class_="snippet-link").text.strip()
-            price = data.find("div", class_="snippet-price-row").text.strip()
-            address = data.find("span", class_="item-address-georeferences-item__content").text.strip()
-            time_published = data.find("div", class_="snippet-date-info").text.strip()
-            link = "https://www.avito.ru" + data.find("a", class_="snippet-link").get("href")
+def parsed_info(data, tag, class_tag):
+    try:
+        result = data.find(tag, class_=class_tag).text.strip()
+    except AttributeError:
+        result = ""
+    return result
+
+
+def write_in_file(info):
+    with open(f"./products/info_product_page_{num}.csv", "w") as file:
+        for data in info:
+            name = parsed_info(data, "a", "snippet-link")
+            price = parsed_info(data, "div", "snippet-price-row")
+            address = parsed_info(data, "span", "item-address-georeferences-item__content")
+            time_published = parsed_info(data, "div", "snippet-date-info")
+            try:
+                link = "https://www.avito.ru" + data.find("a", class_="snippet-link").get("href")
+            except AttributeError:
+                link = ""
             file.write(f"{name},{price},{address},{time_published},{link}\n")
 
 
-start_url = "https://www.avito.ru/moskva/telefony?q=Iphone"
+def print_info(num_page):
+    with open(f"./products/info_product_page_{num_page}.csv", newline='') as csv_file:
+        reader = csv.reader(csv_file, delimiter=",")
+        for row in reader:
+            print(', '.join(row))
 
-for num in range(1, 2): #get_count_pages(get_html(start_url)) + 1):
+
+start_url = "https://www.avito.ru/moskva/telefony?q=Iphone"
+for num in range(1, get_count_pages(get_html(start_url)) + 1):
     url_gen = start_url + f"&p={num}"
     get_page_data(get_html(url_gen))
 
-with open('info_phone.csv', newline='') as csvfile:
-     spamreader = csv.reader(csvfile, delimiter=',')
-     for row in spamreader:
-        print(', '.join(row))
+
+
+
